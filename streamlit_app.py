b0bfbdbdb818c08ea5f206526c65970012042080
ermbrooks <brooks.eric@me.com>
2024-07-24 01:47:44 +0000

    adding models view

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
-import os
+import os, boto3
 
-st.write("test")
-st.write(os.environ["Secret"])
+def get_session():
+    acces_key = os.environ["Access"]
+    secret_key = os.environ["Secret"]
+    session_token = os.environ["Token"]
+    return boto3.Session(acces_key, secret_key, session_token)
+    
+
+def list_available_models():
+    session = get_session()
+    bedrock = session.client("bedrock")
+    models = bedrock.list_foundation_models()
+    return models
+
+
+
+st.write(list_available_models())
