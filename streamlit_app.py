75fcc52bfba28c65f275cfd5983771c458bb136a
ermbrooks <brooks.eric@me.com>
2024-07-25 11:35:37 +0000

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,15 @@
 import streamlit as st
-import authenticate
 
-# (client, id, secret, uri) = utils.get_cognito_info()
-# authenticator = CognitoHostedUIAuthenticator(client, id, secret, use_cookies=False)
-
-# is_logged_in = authenticator.login()
-# if not is_logged_in:
-#     print("Login failed")
-
-# st.write(utils.list_available_models())
-
-# Check authentication when user lands on the home page.
+import authenticate as authenticate
 
 st.set_page_config(
     page_title="Home",
     page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
 
+# Check authentication when user lands on the home page.
 authenticate.set_st_state_vars()
 
 # Add login/logout buttons
@@ -24,3 +17,27 @@ if st.session_state["authenticated"]:
     authenticate.button_logout()
 else:
     authenticate.button_login()
+
+# st.session_state
+
+# with st.echo("below"):
+from st_pages import add_page_title, show_pages, Page
+
+show_pages(
+    [
+        Page("streamlit_app.py", "Home"),
+    ]
+)
+
+add_page_title()  # Optional method to add title and icon to current page
+# Also calls add_indentation() by default, which indents pages within a section
+
+st.write("""
+         Welcome,
+         
+         This app is an example of how to get User Authentication and page wise authorization in a Streamlit multi-page app using AWS Cognito 🚀! 
+         """)
+
+
+
+
