884c2650f4df9f17b3ae0783f3809cd5b539a5b5
ermbrooks <brooks.eric@me.com>
2024-07-26 00:46:05 +0000

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,6 +69,15 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+
 with st.sidebar:
     st.write("sidebar text")
     st.write("sidebar text 2")
